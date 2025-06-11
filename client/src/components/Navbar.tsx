d0264303b3e408537c28b21c3a1c435bb87cd478
Eldrish <eldrish.ramosperez@gmail.com>
2025-06-11 16:13:42 -0700

    added more navbar components, 3 options but only 1 (My SOPs) is clickable at the moment

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar: React.FC = () => (
@@ -12,6 +13,20 @@ const Navbar: React.FC = () => (
         <span className="sop-navbar-highlight">track</span>, and{' '}
         <span className="sop-navbar-highlight">master</span> your Standard Operating Procedures.
       </p>
+      {/* {NAVIGATION LINKS BELOW} */}
+      <div className="sop-navbar-links" style={{ display: 'flex', justifyContent: 'center', gap: '2rem', marginTop: '1.5rem', zIndex: 1, position: 'relative' }}>
+        <Link to="/" className="sop-navbar-link sop-navbar-highlight" style={{ textDecoration: 'none', fontWeight: 600 }}>
+          My SOPs
+        </Link>
+        <span className="sop-navbar-link" style={{ color: '#fff', fontWeight: 600, cursor: 'pointer', opacity: 0.8 }}>
+          SOPs
+        </span>
+        <span className="sop-navbar-link" style={{ color: '#fff', fontWeight: 600, cursor: 'pointer', opacity: 0.8 }}>
+          Support
+        </span>
+      </div>
+      <div className="sop-navbar-circle-left" />
+      <div className="sop-navbar-circle-right" />
     </div>
   </nav>
 );
