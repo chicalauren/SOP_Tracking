1e897cbdc7bb6d076e92a8251b1542767d9233f0
Eldrish <eldrish.ramosperez@gmail.com>
2025-06-11 17:51:42 -0700

    added Roles to userSchema in User.ts (server folder)

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -3,6 +3,12 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ['User','Reporter','Auditor','Administrator'],
+    default: 'user',
+    required: true,
+  }
 });
 
 export default mongoose.model('User', userSchema);
